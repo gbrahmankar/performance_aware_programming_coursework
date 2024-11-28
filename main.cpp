3c2ff14115dce77da0e923f4d19886480ef79ab7
gbrahmankar <gauravb14.comp@gmail.com>
2024-11-29 00:27:01 +0530

    refactor : part_two cask

--- main.cpp
+++ main.cpp
@@ -1,20 +1,14 @@
 #include "part_one/8086_decoder_simulator.h"
 
-int main(int argc, char* argv[])
+void executePartOne(int argc, char* argv[])
 {
     using namespace PartOne;
 
-    if (argc < 2) 
-    {
-        std::cerr << "usage : " << argv[0] << " <binary_file_to_disassemble>" << '\n';
-        return 1;
-    }
-
-    std::ifstream file(argv[1], std::ios::binary);
+    std::ifstream file(argv[2], std::ios::binary);
     if (!file.is_open()) 
     {
-        std::cerr << "failed to open the file. file_name=" << argv[1] << '\n';
-        return 1;
+        std::cerr << "failed to open the file. file_name=" << argv[2] << '\n';
+        return;
     }
 
     uint16_t totalProgramCost = 0;
@@ -30,7 +24,7 @@ int main(int argc, char* argv[])
 
         decodeFirstInstructionByte(firstByteBitset, file, ongoingInstructionMetadata); 
 
-        if (argc >= 3 && std::string(argv[2]) == "simulate")
+        if (argc >= 3 && std::string(argv[3]) == "simulate")
         {
             simulateInstruction(ongoingInstructionMetadata);
             totalProgramCost += ongoingInstructionMetadata.totalInstructionCost;
@@ -50,6 +44,33 @@ int main(int argc, char* argv[])
     std::cout << registers.getAllRegisterFileStream(false).str() << '\n';
 
     std::cout << "TotalProgramCost=" << totalProgramCost << '\n';
+}
 
-    return 0;    
+void executePartTwo(int argc, char* argv[])
+{
+}
+
+int main(int argc, char* argv[])
+{
+    if (argc < 2)
+    {
+        std::cerr << "usage : " << argv[0] << "<what_part_to_execute. eg : part_one/part_two>" << '\n';
+        return 1;
+    }
+
+    if (std::string(argv[1]) == "part_one")
+    {
+        if (argc < 3)
+        {
+            std::cerr << "usage : " << argv[0] << " part_one <binary_encoded_instruction_stream file>" << '\n';
+            return 1;
+        }
+        executePartOne(argc, argv);
+    }
+    else if (std::string(argv[1]) == "part_two")
+    {
+        executePartTwo(argc, argv);
+    }
+
+    return 0;
 }
