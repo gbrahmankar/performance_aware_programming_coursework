60ef4af50ba2774d6189f3dc1b06097e092a42ff
Gaurav Brahmankar <gaurav.brahmankar@ubisoft.com>
2025-04-07 17:37:33 +0530

    part_03, video_15 : hw started.

--- pap_odin/asm/chapter15_cache_bw_tests.asm
+++ pap_odin/asm/chapter15_cache_bw_tests.asm
@@ -1,15 +1,33 @@
 ; > nasm.exe -f win64 -g chapter15_cache_bw_tests.asm -o chapter15_cache_bw_tests.obj
 ; > lib.exe chapter15_cache_bw_tests.obj /out:chapter15_cache_bw_tests.lib
 
-global aligned_at_64_bytes
+global cache_size_bw_test 
 
 section .text
 
-aligned_at_64_bytes :
+cache_size_bw_test :
+    xor r9, r9
     xor rax, rax
 align 64
 .loop:
-    inc rax
-    cmp rax, rcx
-    jb .loop
+    vmovdqu ymm0, [rax]
+    vmovdqu ymm0, [rax + 16]
+    vmovdqu ymm0, [rax + 32]
+
+    vmovdqu ymm0, [rax + 48]
+    vmovdqu ymm0, [rax + 64]
+    vmovdqu ymm0, [rax + 80]
+    
+    vmovdqu ymm0, [rax + 96]
+    vmovdqu xmm0, [rax + 104]
+    vmovdqu ymm0, [rax + 112]
+
+    add r9, 0x100
+    and r9, r8
+    
+    mov rax, rdx
+    add rax, r9
+
+    sub rcx, 0x100
+    jnz .loop
     ret
--- pap_odin/part_three/chapter15_cache_bw_tests.odin
+++ pap_odin/part_three/chapter15_cache_bw_tests.odin
@@ -5,4 +5,7 @@ foreign import cache_bw_tests "../asm/chapter15_cache_bw_tests.lib"
 
 @(default_calling_convention="c")
 foreign cache_bw_tests {
+	cache_size_bw_test :: proc(count: u64, data: ^u8, mask: u64) -> u64 --- 
 }
+
+cache_size_bw_test_asm :: cache_size_bw_test
